6e0f21b620e7d4c16aa3142e188c33a89bedb4d3
ThanhThanhBinh <loraltumblr@gmail.com>
2022-12-07 19:52:21 +0000

    Finsih frequency

--- frequencies.py
+++ frequencies.py
@@ -4,4 +4,10 @@
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+    for index in items:
+        current_value = str(index)
+        if current_value in frequencies:
+            frequencies[current_value] +=1
+        else:
+            frequencies[current_value] =1
     return frequencies
